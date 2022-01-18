90b6b9d5bc0961820c4892648704d9bc79f3329c
usman747 <95546951+usman747@users.noreply.github.com>
2022-01-18 22:21:41 +0100

    Updated architecture
    Separate process for state (clk, rst), and separate for state transition

--- Exercise5/unlocker.vhd
+++ Exercise5/unlocker.vhd
@@ -20,10 +20,18 @@ begin  -- architecture rtl
 process(clk_i)
 begin
     if (rising_edge(clk_i)) then
+        if rst_i='1' then
+            cur_state_s <= s0;
+        else
         cur_state_s <= next_state_s;
-            if rst_i='1' then
-                cur_state_s <= s0;
-            else
+        end if;
+    end if;
+end process; 
+
+
+--next state logic
+process(psw_i, cur_state_s)
+begin
                 case cur_state_s is
                      when s0 =>        --when current state is "s0"
                          if(psw_i ='0') then
@@ -69,10 +77,73 @@ begin
   
                        when others => null;
                  end case;
-             end if;
-         end if;
-     end process;
+end process;
      
-     unlock_o <= '1' when (cur_state_s = s4) and (psw_i='0')
-     else '0';
+
+--output     
+unlock_o <= '1' when (cur_state_s = s4) and (psw_i='0')
+else '0';
+
+
+-- wrong implementation below...
+--process(clk_i)
+--begin
+--    if (rising_edge(clk_i)) then
+--        cur_state_s <= next_state_s;
+--            if rst_i='1' then
+--                cur_state_s <= s0;
+--            else
+--                case cur_state_s is
+--                     when s0 =>        --when current state is "s0"
+--                         if(psw_i ='0') then
+--                              next_state_s <= s0;
+--                         else
+--                              next_state_s <= s1;
+--                         end if;
+
+--                     when s1 =>        --when current state is "s1"
+--                         if(psw_i ='0') then
+--                              next_state_s <= s0;
+--                         else
+--                              next_state_s <= s2;
+--                         end if;
+
+--                     when s2 =>        --when current state is "s1"
+--                         if(psw_i ='0') then
+--                              next_state_s <= s3;
+--                         else
+--                              next_state_s <= s2;
+--                         end if;
+                         
+--                     when s3 =>        --when current state is "s1"
+--                         if(psw_i ='0') then
+--                              next_state_s <= s0;
+--                         else
+--                              next_state_s <= s4;
+--                         end if;
+                         
+--                     when s4 =>        --when current state is "s1"
+--                         if(psw_i ='0') then
+--                              next_state_s <= s5;
+--                         else
+--                              next_state_s <= s2;
+--                         end if;
+
+--                     when s5 =>        --when current state is "s1"
+--                         if(psw_i ='0') then
+--                              next_state_s <= s0;
+--                         else
+--                              next_state_s <= s1;
+--                         end if;
+  
+--                       when others => null;
+--                 end case;
+--             end if;
+--         end if;
+--     end process;
+     
+--     unlock_o <= '1' when (cur_state_s = s4) and (psw_i='0')
+--     else '0';
+--
+
 end architecture rtl;
